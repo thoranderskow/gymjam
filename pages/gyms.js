36991ab10eddad207a0cecd9f6bc3b64cda458e2
thoranderskow <tanderskow@gmail.com>
2020-06-22 20:48:47 -0700

    moved formatting of comments

--- pages/gyms.js
+++ pages/gyms.js
@@ -1,17 +1,74 @@
 import fetch from 'isomorphic-unfetch'
 import React, { Component } from 'react'
 
+function Create_commentbox(props) {
+  return(
+    <div className='flex'>
+      <div className='comlev'>
+        <div className='bold'>crowd level: {props.c_level}</div>
+        <div className='bold'>Racks available? {props.racks ? 'yes' : 'no'}</div>
+      </div>
+      <div className='unemph'>{props.com}</div>
+      <style jsx>{`
+        .flex {
+          display: flex;
+          border-right: 2px solid black;
+          border-left: 2px solid black;
+          border-top: 2px solid black;
+          border-bottom: 0;
+          justify-content: space-evenly;
+          flex-direction: column;
+          margin-bottom: 5px;
+        }
+        .flex:last-child{
+          border-bottom: 2px solid black;
+        }
+        .flex:nth-child(odd){
+          background-color: #CCC;
+        }
+        .comlev {
+          display: flex;
+          justify-content: space-evenly;
+          margin-bottom: 5px;
+        }
+        .bold {
+          font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+        }
+        .unemph {
+          font-family: Charcoal, sans-serif;
+          line-height: 150%;
+          font-size: 16pt;
+        }
+        `}
+      </style>
+    </div>
+  );
+}
+
+function Show_five(props) {
+  const arr = props.arr.slice(0,5);
+  const comments = arr.map((obj) =>
+    <Create_commentbox key={obj.id} com={obj.comment} c_level={obj.c_level} racks={obj.r_avail} />
+  );
+  return (
+    <div>
+      <div>{comments}</div>
+    </div>
+  )
+}
+
 export default class extends React.Component {
   static async getInitialProps() {
     const res = await fetch('http://localhost:3000/api/gym')
     const data = await res.json()
-    console.log(data);
-    const newdata = JSON.stringify(data);
-    return { newdata }
+    //array is reversed for most recent to show up
+    const arr = data.comments.reverse();
+    return { arr }
   }
   render() {
+    console.log(this.props.arr);
     return(
-      <h1>{this.props.newdata}</h1>
+      <Show_five arr={this.props.arr}/>
     )
   }
 }
